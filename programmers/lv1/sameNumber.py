f92c749b6faa49dd5f58b1d1f0d9154f50aef365
DongJae <dkekzhs5@naver.com>
2022-08-17 16:43:47 +0900

    같은 숫자는 싫어

---
+++ programmers/lv1/sameNumber.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr ) -1):
+        if(arr[i] == arr[i+1]):
+            pass
+        else :
+            answer.append(arr[i])
+    answer.append(arr[len(arr) -1])
+    return answer
