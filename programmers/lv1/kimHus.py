b653f7d647ac4ed916477d28351a0e421d280a99
DongJae <dkekzhs5@naver.com>
2022-08-28 18:47:53 +0900

    서울에서 김서방 찾기

---
+++ programmers/lv1/kimHus.py
@@ -0,0 +1,9 @@
+def solution(seoul):
+    answer = '김서방은 '     
+    for i in range(len(seoul)):
+        if seoul[i] == "Kim":
+            answer += str(i) +"에 있다"
+
+    return answer
+
+solution(["Jane","Kim"])
