2b7f74a10afe20ad7ff846dc68ea674c62b6aec2
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-10-30 19:20:26 +0900

    n^2 배열 자르기

---
+++ programmers/lv2/cut2^n.py
@@ -0,0 +1,11 @@
+def solution(n, left, right):
+    answer = []
+
+    left, right = int(left), int(right)
+    for i in range(left, right+1):
+
+        answer.append(max((i // n), (i % n)) + 1)
+        
+    return answer
+
+print(solution(4,2,5))
