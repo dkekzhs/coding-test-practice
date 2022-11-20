f220961c5a7f616658049a153e49a8f4de36edb1
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-11-20 20:25:52 +0900

    쿠키 바구니

---
+++ programmers/lv4/mrio.py
@@ -0,0 +1,12 @@
+def solution(n, start, end, roads, traps):
+    answer = 0 
+    
+            
+    print(answer)
+
+    return answer
+
+#roads 첫번째는 현재 노드의 번호 두번째는 연결된 노드 3번째는 걸리는 시간
+
+# solution(3,1,3,[[1, 2, 2], [3, 2, 3]],[2])
+solution(4,1,4,[[1, 2, 1], [3, 2, 1], [2, 4, 1]],[2, 3])
