e311d03b07c6393ec7b5882ba1374bfe5d1940cf
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-12-23 20:47:57 +0900

    정수 삼각형 동적프로그래밍

---
+++ programmers/lv4/cookieBucket.py
@@ -0,0 +1,25 @@
+def solution(cookie):
+    answer = 0
+    left,right = 0,0
+    leftSum,rightSum = 0,0
+    for i in range(len(cookie) -1 ):
+        left = i
+        leftSum = cookie[i]
+        right = i+1
+        rightSum = cookie[i+1]
+        while True:
+            if leftSum == rightSum:
+                answer = max(answer, leftSum)
+            if leftSum > rightSum:
+                right += 1
+                if right == len(cookie) : 
+                    break
+                rightSum += cookie[right]
+            else:
+                left -=1 
+                if left < 0 :
+                    break
+                leftSum += cookie[left]
+    return answer
+
+solution([1,1,2,3])
