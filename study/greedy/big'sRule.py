10cee825d8da0bd70f150fff95b2a5765536ebeb
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-12-26 16:57:22 +0900

    그리디 알고리즘 큰 수 수열

--- study/greedy/big'sRule.py
+++ study/greedy/big'sRule.py
@@ -35,10 +35,27 @@ def greedy_big2(i1,i2):
         m-= 1
     print(result)
 
+
+#수열로 구현 
+def greedy_big3(i1,i2):
+    n,m,k = map(int,i1.split())
+    data = list(map(int,i2.split()))
+    data.sort()
+    fir = data[len(data) - 1]
+    sec = data[len(data) - 2]
+    result = 0
+
+    count = int(m/ (k+1)) * k
+    count += m % (k+1)
+    result += count * fir
+    result += (m - count) *sec
+    print(result)
+
 i1 = "5 8 3"
 i2 = "2 4 5 4 6"
 
 greedy_big2(i1,i2)
+greedy_big3(i1,i2)
 
 n,m,k = 5,8,3
 data = [2,4,5,4,6]
