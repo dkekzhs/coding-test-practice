a2076d37ebc4c807447a4439c7c8315c4762a69c
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-01-04 21:36:37 +0900

    이진 탐색

--- study/search/binary.py
+++ study/search/binary.py
@@ -1,9 +1,11 @@
+
 def binary_search(a,target,start,end):
     if start > end : 
         return None
     mid = (start+end)//2
-    if mid == target:
+    if a[mid] == target:
         return mid
+
     elif a[mid] > target:
         return binary_search(a,target,start,mid-1)
     else:
@@ -12,4 +14,6 @@ def binary_search(a,target,start,end):
 
 b = [0,3,8,11,23,34]
 find = 23
-print(binary_search(b,find,0,len(b)))
+result = binary_search(b,find,0,len(b))
+
+print(result)
