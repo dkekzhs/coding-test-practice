67beb35336e878e9e82d351af659e81c0d0c8597
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-02-08 20:40:14 +0900

    [72414] 광고삽입

--- programmers/lv3/[72414]InsetAdv.py
+++ programmers/lv3/[72414]InsetAdv.py
@@ -1,37 +1,31 @@
-def timeSet(time):
-    h,h2 = divmod(time,3600)
-    m,m2 = divmod(h2,60)
-    s,s2 = divmod(m2,60)
-    if len(str(h)) == 1 : 
-        h ="0"+str(h)
-    if len(str(m)) == 1 : 
-        m ="0"+str(m)
-    if len(str(s2)) == 1 : 
-        s2 ="0"+str(s)        
-    return str(h)+":"+str(m)+":"+str(s2)
-def ssplit(time):
+def timeSplit(time):
     h,m,s = list(map(int,time.split(":")))
-    return h*60*60 + m*60 + s
+    return (h*60*60)+(m*60)+s
+def timeTrans(time):
+    h = time//3600
+    time %=3600
+    m = time//60
+    time %=60
+    return str(h).zfill(2)+":"+str(m).zfill(2)+":"+str(time).zfill(2)
 def solution(play_time, adv_time, logs):
-    view_arr = [0] * (ssplit(play_time) + 1)
+    view_arr = [0] * (timeSplit(play_time) + 1)
     
     for i in logs:
         start, end = i.split("-")
-        start_sec = ssplit(start)
-        end_sec = ssplit(end)
+        start_sec = timeSplit(start)
+        end_sec = timeSplit(end)
         view_arr[start_sec] +=1
         view_arr[end_sec] -=1
     
     for j in range(1,len(view_arr)):
         view_arr[j] += view_arr[j-1]
-    
     prefix_sum = [0]
     total = 0
     for i in range(len(view_arr)):
         total += view_arr[i]
         prefix_sum.append(total)  
-    adv_time = ssplit(adv_time)
-
+    adv_time = timeSplit(adv_time)
+    
     max_time = 0
     answer = 0
     for i in range(len(view_arr)):
@@ -41,7 +35,7 @@ def solution(play_time, adv_time, logs):
         if insert_time > max_time:
             max_time = insert_time
             answer = i
-    return timeSet(answer)
+    return timeTrans(answer)
 
 
 solution("99:59:59","25:00:00",["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"])
