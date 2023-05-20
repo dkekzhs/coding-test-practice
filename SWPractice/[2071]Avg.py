f94bca065f11ff498ad550b5bb1fb405ca32b5be
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-05-20 18:30:57 +0900

    [2071] 평균값 구하기

---
+++ SWPractice/[2071]Avg.py
@@ -0,0 +1,10 @@
+def avg(aa):
+    sum = 0
+    for i in range(len(aa)):
+        sum += aa[i]
+    return int(round(sum/ len(aa),0))
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    aa = list(map(int,input().split()))
+    print("#"+str(test_case)+" "+ str(avg(aa)))
