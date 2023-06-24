d34dca7ae006407d0d40ebc90f92b180a53f55f3
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-06-24 18:36:28 +0900

    [1000] A+B

--- java/src/main/java/org/example/백준기초/aAddB_1000.java
+++ java/src/main/java/org/example/백준기초/aAddB_1000.java
@@ -1,7 +1,7 @@
-package org.example.백준기초100제;
+package org.example.백준기초;
 
 import java.util.Scanner;
-public class aAddB_100 {
+public class aAddB_1000 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
