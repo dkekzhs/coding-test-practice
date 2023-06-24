16f05e7b61385c12ca6bba35bbca7309055b89a7
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2023-06-24 18:36:07 +0900

    [2557] Hello World

--- java/src/main/java/org/example/백준기초/Hello_world_2557.java
+++ java/src/main/java/org/example/백준기초/Hello_world_2557.java
@@ -1,6 +1,6 @@
-package org.example.백준기초100제;
+package org.example.백준기초;
 
-public class Hellow_world_2557 {
+public class Hello_world_2557 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
